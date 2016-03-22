f1edda1a3ece084ce8ec2f36afe9c76a137370a3
travis kirton <travis@c4ios.com>
2016-03-22 01:43:37 -0700

    Adds layout for collectionview controller

---
+++ Peripheral/Peripheral/ScheduleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleViewController.swift
+//  Peripheral
+//
+//  Created by travis on 2016-03-21.
+//  Copyright © 2016 C4. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScheduleViewController: UICollectionViewController {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView?.registerClass(EventCell.self, forCellWithReuseIdentifier: "EventCell")
+    }
+}
