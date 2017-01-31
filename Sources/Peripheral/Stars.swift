4668409c90aa6d8df6d5a89ea9d8cc09abb7e52b
travis kirton <travis@c4ios.com>
2017-01-30 22:20:59 -0800

    Updates storyboard

--- Sources/Peripheral/Stars.swift
+++ Sources/Peripheral/Stars.swift
@@ -26,8 +26,14 @@ open class Stars: UniverseController, ScrollDelegate, GCDAsyncSocketDelegate {
     static let constellationCount = 28
     static let maxWidth = CGFloat(constellationCount) * CGFloat(frameCanvasWidth)
     var bigStarsViewController: BigStarsViewController?
+    var smallStarsViewController: SmallStarsViewController?
 
     func initializeCollectionView() {
+        inititalizeSmallStars()
+//        inititalizeBigStars()
+    }
+
+    func inititalizeBigStars() {
         let storyboard = UIStoryboard(name: "BigStarsViewController", bundle: nil)
         bigStarsViewController = storyboard.instantiateViewController(withIdentifier: "BigStarsViewController") as? BigStarsViewController
         guard bigStarsViewController != nil else {
@@ -38,6 +44,18 @@ open class Stars: UniverseController, ScrollDelegate, GCDAsyncSocketDelegate {
 
         canvas.add(bigStarsViewController?.collectionView)
         bigStarsViewController?.scrollDelegate = self
+    }
+
+    func inititalizeSmallStars() {
+        let storyboard = UIStoryboard(name: "SmallStarsViewController", bundle: nil)
+        smallStarsViewController = storyboard.instantiateViewController(withIdentifier: "SmallStarsViewController") as? SmallStarsViewController
+        guard smallStarsViewController != nil else {
+            print("Collection view could not be instantiated from storyboard.")
+            return
+        }
+        smallStarsViewController?.collectionView?.dataSource = SmallStarsDataSource.shared
+
+        canvas.add(smallStarsViewController?.collectionView)
     }
 
     open override func receivePacket(_ packet: Packet) {
